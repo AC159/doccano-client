3f4b8dd5962767f1f804018d26befe85f3117ed3
Anastassy Cap <anacap100@gmail.com>
2022-06-20 17:39:30 -0400

    commons labels are now merged together

---
+++ merge_models.py
@@ -0,0 +1,43 @@
+
+from allennlp.predictors.predictor import Predictor
+
+ner_fine_grained = Predictor.from_path(
+    "https://storage.googleapis.com/allennlp-public-models/fine-grained-ner.2021-02-11.tar.gz")
+ner_elmo = Predictor.from_path(
+    "https://storage.googleapis.com/allennlp-public-models/ner-elmo.2021-02-12.tar.gz")
+
+# This function gives priority to the fine grained model, In case the word doesn't have a tag, we take the elmo tag
+# for this word
+def merge_results(fine_grained, elmo):
+    result = []
+    for i in range(len(fine_grained)):
+        if fine_grained[i] == 'O':
+            result.append(elmo[i])
+        else:
+            result.append(fine_grained[i])
+    return result
+
+# This function merges the words whose tags are between B-I-L, It will need a small change to remove the prefix and
+# and return the tag instead of the words
+def extract_person_and_place(fine_grained, elmo):
+    output = []
+    for i in range(len(fine_grained['tags'])):
+        word = []
+        if fine_grained['tags'][i].startswith('U-') or elmo['tags'][i].startswith('U-'):
+            output.append(fine_grained['words'][i])
+        elif fine_grained['tags'][i].startswith('B-') or elmo['tags'][i].startswith('B-'):
+            word.append(fine_grained['words'][i])
+        elif fine_grained['tags'][i].startswith('I-') or elmo['tags'][i].startswith('I-'):
+            word.append(fine_grained['words'][i])
+        elif fine_grained['tags'][i].startswith('L-') or elmo['tags'][i].startswith('L-'):
+            word.append(fine_grained['words'][i])
+            output.append(' '.join(word))
+            word = []
+    return output
+
+def predict(sentence):
+    fine_grained_output = ner_fine_grained.predict(sentence=sentence)
+    elmo_output = ner_elmo.predict(sentence=sentence)
+    result = merge_results(fine_grained_output['tags'], elmo_output['tags'])
+    output = extract_person_and_place(fine_grained_output, elmo_output)
+    return output
--- models.py
+++ models.py
@@ -8,16 +8,41 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter import Tk
 
 
-def predict(sentence):
+def mergeCommonLabels(listOfLabels):
+    newList = list()
 
+    length = len(listOfLabels)
+    labelStartIndex = 0
+    if length > 0:
+        labelStartIndex = listOfLabels[0][0]
+
+    for index, labelList in enumerate(listOfLabels):
+
+        labelEndIndex = labelList[1]
+        labelName = labelList[2]
+
+        """
+        If the next label does not have the same name or is not adjacent or we are at the end of the list of labels,
+        we change the cursor positions and merge
+        """
+        if index == length - 1 or labelEndIndex + 1 != listOfLabels[index + 1][0] or labelName != \
+                listOfLabels[index + 1][2]:
+            newList.append([labelStartIndex, labelEndIndex, labelName])
+            if index != length - 1:
+                labelStartIndex = listOfLabels[index + 1][0]
+
+    return newList
+
+
+def predict(sentence, model):
     # These two models use different set of tags, It will be useful to check both
-    fine_grained_output = ner_fine_grained.predict(sentence=sentence)
+    fine_grained_output = model.predict(sentence=sentence)
     words = fine_grained_output['words']
     labels = fine_grained_output['tags']
-    print(f"Words: \n {words}")
-    print(f"Labels: \n {labels}")
-    print(f'Words length: {len(words)}')
-    print(f'Labels length: {len(labels)}')
+    # print(f"Words: \n {words}")
+    # print(f"Labels: \n {labels}")
+    # print(f'Words length: {len(words)}')
+    # print(f'Labels length: {len(labels)}')
     # print(f"Full output: \n {fine_grained_output}")
 
     # elmo_output = ner_elmo.predict(sentence=sentence)
@@ -25,8 +50,7 @@ def predict(sentence):
     return words, labels
 
 
-def main():
-
+def main(fine_grained):
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     inputFilePath = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
     print(inputFilePath)
@@ -53,27 +77,39 @@ def main():
         startIndexOfLabel = 0
         endIndexOfLabel = 0
         listOfLabels.clear()
-        words, labels = predict(sentence)
+        words, labels = predict(sentence, fine_grained)
 
         for index, currentLabel in enumerate(labels):
+
+            splitLabel = currentLabel.split('-')[-1]
+
+            if currentLabel == 'O' and index != len(labels) - 1:
+                startIndexOfLabel = startIndexOfLabel + len(words[index]) + 1
+                endIndexOfLabel = endIndexOfLabel + len(words[index]) + 1
+                continue
+
             if index == len(labels) - 1 or currentLabel != labels[index + 1]:
                 # the next label is different so we record this label in the list together with the indexes
-                listOfLabels.append([startIndexOfLabel, endIndexOfLabel + len(words[index]), currentLabel])
+                if currentLabel != 'O':
+                    listOfLabels.append([startIndexOfLabel, endIndexOfLabel + len(words[index]), splitLabel])
                 if index == len(labels) - 1:
-                    data = {"data": " ".join(words), "label": listOfLabels}
+                    # Once we are done with this sentence, we need to merge all common labels in the list
+                    mergedLabels = mergeCommonLabels(listOfLabels)
+
+                    data = {"data": " ".join(words), "label": mergedLabels}
                     print(json.dumps(data))
                     outputFile.write(json.dumps(data) + "\n")
                 # if words[index] in charsThatNeedNoSpaces:
                 #     startIndexOfLabel = endIndexOfLabel + len(words[index])
                 # else:
-                startIndexOfLabel = endIndexOfLabel + len(words[index]) + 1  # +1 accounts for the space at the end of the word
+                startIndexOfLabel = endIndexOfLabel + len(
+                    words[index]) + 1  # +1 accounts for the space at the end of the word
 
             # if words[index] in charsThatNeedNoSpaces:
             #     endIndexOfLabel = endIndexOfLabel + len(words[index])
             # else:
-            endIndexOfLabel = endIndexOfLabel + len(words[index]) + 1  # +1 accounts for the space at the end of the word
-
-    print(f'List of labels: {listOfLabels}')
+            endIndexOfLabel = endIndexOfLabel + len(
+                words[index]) + 1  # +1 accounts for the space at the end of the word
 
     outputFile.close()
 
@@ -93,9 +129,10 @@ def main():
 
 
 if __name__ == '__main__':
-    ner_fine_grained = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/fine-grained-ner.2021-02-11.tar.gz")
+    ner_fine_grained = Predictor.from_path(
+        "https://storage.googleapis.com/allennlp-public-models/fine-grained-ner.2021-02-11.tar.gz")
 
     # ner_elmo = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/ner-elmo.2021-02-12.tar.gz")
 
-    main()
+    main(ner_fine_grained)
     # predict("EternalBlue is a computer exploit developed by the U.S. Exploit National Security Agency (NSA).")
