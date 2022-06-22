1d79d565d1621f06a38feeb4ebd3c9d48f430624
Anastassy Cap <anacap100@gmail.com>
2022-06-21 21:51:52 -0400

    added dropdown and file choosing menu are on the same gui window now

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from merge_models import merge_results
 from doccano_api_client import DoccanoClient
 
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from tkinter import Tk
+from tkinter import Tk, ttk, StringVar, W, E
 
 
 def mergeCommonLabels(listOfLabels):
@@ -44,10 +44,124 @@ def predict(sentence, model):
     return words, labels
 
 
+# ----------------------------------------------
+class ProjectSettings:
+
+    def __init__(self):
+        self.inputFilePath = ""
+        self.outputFilePath = ""
+        self.outputFileName = ""
+
+        self.doccanoProjects = list()
+        self.selectedProjectNameFromDropDown = ""  # name of the selected project in the 'doccanoProjects' variable
+        self.frame = None  # Contains the ttk frame
+
+    def getIdOfSelectedProject(self):
+        for project in self.doccanoProjects['results']:
+            if self.selectedProjectNameFromDropDown == project['name']:
+                return project['id']
+        return -1
+
+    def validParameters(self):
+        return self.inputFilePath != "" and self.outputFilePath != "" and len(self.doccanoProjects) > 0 and \
+               self.selectedProjectNameFromDropDown != ""
+
+    def __str__(self):
+        return f'Input file: {self.inputFilePath}\n Output file: {self.outputFileName}\n ' \
+               f'Selected Doccano Project: {self.selectedProjectNameFromDropDown}\n '
+
+
+def choose_input_file(projectSettings):
+    filePath = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    print(filePath)
+    fileName = filePath.split("/")[-1]
+
+    projectSettings.inputFilePath = filePath
+
+    # update the frame in the GUI that we have selected an input file
+    ttk.Label(projectSettings.frame, text=f'{fileName}').grid(column=1, row=1)
+
+
+def choose_output_file(projectSettings):
+    # write processed text to a file
+    outputFilePath = asksaveasfilename()
+    outputFileName = outputFilePath.split("/")[-1]
+    print(outputFilePath)
+    print(outputFileName)
+    projectSettings.outputFilePath = outputFilePath
+    projectSettings.outputFileName = outputFileName
+
+    # update the frame in the GUI that we have selected an output file
+    ttk.Label(projectSettings.frame, text=f'{outputFileName}').grid(column=1, row=2)
+
+
+def createFrame(rootWindow, projectSettings):
+    frame = ttk.Frame(rootWindow)
+
+    frame.columnconfigure(0, weight=3)
+    frame.columnconfigure(1, weight=1)
+    frame.columnconfigure(2, weight=1)
+
+    # Dropdown menu
+    projectNames = [project['name'] for project in projectSettings.doccanoProjects['results']]
+    ttk.Label(frame, text='Select Doccano project: ').grid(column=0, row=0, sticky=W)
+
+    # this must be set after the main window has been created
+    projectSettings.selectedProjectNameFromDropDown = StringVar()
+    projectSettings.selectedProjectNameFromDropDown.set(projectNames[0])
+    dropdown = ttk.OptionMenu(frame, projectSettings.selectedProjectNameFromDropDown, projectNames[0], *projectNames)
+    dropdown.grid(column=2, row=0)
+
+    # Select input file
+    ttk.Label(frame, text='Select input file: ').grid(column=0, row=1, sticky=W)
+    ttk.Button(frame, text="Browse", command=lambda: choose_input_file(projectSettings)).grid(column=2, row=1, sticky=E)
+
+    # Select output file
+    ttk.Label(frame, text='Select output file (JSONL extension): ').grid(column=0, row=2, sticky=W)
+    ttk.Button(frame, text="Browse", command=lambda: choose_output_file(projectSettings)).grid(column=2, row=2, sticky=E)
+
+    # ok button at the bottom of the frame
+    ttk.Button(frame, text='Done', command=lambda: rootWindow.destroy()).grid(column=2, row=5, pady=50, sticky=E)
+
+    return frame
+
+
 def main(fine_grained, elmo):
-    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
-    inputFilePath = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
-    print(inputFilePath)
+
+    doccano_client = DoccanoClient(
+        'http://localhost:8000',
+        'admin',
+        'admin'
+    )
+
+    projectList = doccano_client.get_project_list()
+
+    projectSettings = ProjectSettings()
+    projectSettings.doccanoProjects = projectList
+
+    mainWindow = Tk()
+    mainWindow.title('Project configuration')
+    mainWindow.geometry("450x175")
+
+    mainWindow.columnconfigure(0, weight=1)
+
+    projectSettings.frame = createFrame(mainWindow, projectSettings)
+    projectSettings.frame.grid(column=0, row=0)
+    mainWindow.mainloop()  # show the window until the user selects all files and clicks 'done'
+
+    # We should now have all the files paths as well as the project chosen by the user
+    projectSettings.selectedProjectNameFromDropDown = projectSettings.selectedProjectNameFromDropDown.get()
+    print(projectSettings)
+
+    # Sanity check that we have all the available information needed
+    if not projectSettings.validParameters():
+        root = Tk()
+        message = ttk.Label(root, text='Input parameters are not valid!')  # todo: make the error message more precise
+        message.pack(padx=15, pady=15)
+        root.mainloop()
+        exit(0)
+
+    inputFilePath = projectSettings.inputFilePath
 
     file = open(inputFilePath, "r")
     fullText = file.read()
@@ -56,10 +170,8 @@ def main(fine_grained, elmo):
     listOfLabels = list()
 
     # write processed text to a file
-    outputFilePath = asksaveasfilename()
-    outputFileName = outputFilePath.split("/")[-1]
-    print(outputFilePath)
-    print(outputFileName)
+    outputFilePath = projectSettings.outputFilePath
+    outputFileName = projectSettings.outputFileName
     outputFile = open(outputFilePath, "a")
     outputFile.truncate(0)  # clear contents of the file starting at the beginning of the file
 
@@ -115,17 +227,11 @@ def main(fine_grained, elmo):
     outputFile.close()
 
     # upload text file to the doccano instance
-    doccano_client = DoccanoClient(
-        'http://localhost:8000',
-        'admin',
-        'admin'
-    )
-
     r_json_upload = doccano_client.post_doc_upload(
-        project_id=3,
-        file_name='output.jsonl',
+        project_id=projectSettings.getIdOfSelectedProject(),
+        file_name=outputFileName,
         column_data="data",
-        file_path='C:/Users/Anastassy Cap/Downloads/')
+        file_path=outputFilePath.replace(outputFileName, ''))
     print(r_json_upload)
 
 
